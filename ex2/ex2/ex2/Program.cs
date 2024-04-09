5dcb3477e67297dcb14ef6a49addec685cfec59b
yasaman <101281998+yasamanBD@users.noreply.github.com>
2024-04-09 21:25:31 +0330

    Add files via upload

---
+++ ex2/ex2/ex2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Management;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var watcher = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 2")))
+        {
+            watcher.EventArrived += (sender, eventArgs) =>
+            {
+                Console.WriteLine("USB device connected!");
+                Process.Start("mspaint");
+            };
+            watcher.Start();
+
+            Console.WriteLine("Listening for USB device connection events. Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
